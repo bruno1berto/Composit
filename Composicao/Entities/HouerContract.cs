04a960a79522454b92fd2707b49407f2c8347cab
1bertoMaia <bruno1berto@hotmail.com>
2020-04-10 19:36:15 -0300

    Promeiro commit. OBS: Falta implementar a classe program.

---
+++ Composicao/Entities/HouerContract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composicao.Entities
+{
+    class HouerContract
+    {
+        public DateTime Date { get; set; }
+        public double  ValuePerHouer { get; set; }
+        public int Hours { get; set; }
+
+        public HouerContract(DateTime date, double valuePerHouer, int hours)
+        {
+            Date = date;
+            ValuePerHouer = valuePerHouer;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHouer;
+        }
+    }
+}
---
+++ Composicao/Entities/Worker.cs
@@ -0,0 +1,49 @@
+﻿using Composicao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composicao.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department  { get; set; }
+        public List<HouerContract> Contracts { get; set; } = new List<HouerContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HouerContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void RemoveContract(HouerContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+        public double Incomer(int yaer, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HouerContract contract in Contracts) 
+            {
+                if (contract.Date.Year == yaer && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
